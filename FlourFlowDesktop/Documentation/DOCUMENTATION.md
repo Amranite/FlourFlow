461e8845023721e1385e560b1864185e49863952
Amranite <locker-devise-0i@icloud.com>
2024-10-27 23:59:17 +0100

    feat: add try catch

--- FlourFlowDesktop/Documentation/DOCUMENTATION.md
+++ FlourFlowDesktop/Documentation/DOCUMENTATION.md
@@ -29,12 +29,12 @@ This document outlines the features, installation, instructions, and technologie
 - [x] .NET 8.O WPF Application
 - [x] Entity Framework
 - [x] Database with 3 linked tables
-- [ ] Basic UI in XAML
-- [ ] Fundamental CRUD operations for at least 3 models
+- [x] Basic UI in XAML
+- [x] Fundamental CRUD operations for at least 3 models
 - [ ] Selection/Filter field through a TextBox, ListBox, ... for at least 3 models
 - [ ] Use a LINQ statement and lambda expression for each model
-- [ ] 2/3rds of the controls are linked to a Style that are in a resource file (xaml)
-- [ ] 4/5th of the displayed texts are retrieved from one or more resource files (resx)
+- [x] 2/3rds of the controls are linked to a Style that are in a resource file (xaml)
+- [x] 4/5th of the displayed texts are retrieved from one or more resource files (resx)
 - [ ] C# code is used to manage at least 2 controls
 - [ ] Try/Catch block is used at least once
-- [ ] At least 3 types of containers are used in XAML
+- [x] At least 3 types of containers are used in XAML
--- FlourFlowDesktop/MainWindow.xaml.cs
+++ FlourFlowDesktop/MainWindow.xaml.cs
@@ -21,11 +21,17 @@ namespace FlourFlowDesktop
 	public partial class MainWindow : Window
 	{
 		private readonly IngredientRepository _ingredientRepository;
+		private readonly OrderRepository _orderRepository;
+		private readonly SupplierRepository _supplierRepository;
 		public MainWindow()
 		{
 			InitializeComponent();
 			_ingredientRepository = new IngredientRepository(new FlourFlowContext());
+			_orderRepository = new OrderRepository(new FlourFlowContext());
+			_supplierRepository = new SupplierRepository(new FlourFlowContext());
 			LoadIngredients();
+			LoadOrders();
+			LoadSuppliers();
 		}
 
 		private void LoadIngredients()
@@ -33,18 +39,76 @@ namespace FlourFlowDesktop
 			IngredientsDataGrid.ItemsSource = _ingredientRepository.GetAll();
 		}
 
+		private void LoadOrders()
+		{
+			OrdersDataGrid.ItemsSource = _orderRepository.GetAll();
+		}
+
+		private void LoadSuppliers()
+		{
+			SuppliersDataGrid.ItemsSource = _supplierRepository.GetAll();
+		}
+
 		private void AddIngredient_Click(object sender, RoutedEventArgs e)
 		{
-			var ingredient = new Ingredient
+			try
 			{
-				Name = IngredientNameTextBox.Text,
-				QuantityInStock = decimal.TryParse(QuantityTextBox.Text, out var qty) ? qty : 0m, // Change to decimal
-				Unit = UnitTextBox.Text,
-				PricePerUnit = decimal.TryParse(PriceTextBox.Text, out var price) ? price : 0m // Use '0m' for decimal
-			};
+				var ingredient = new Ingredient
+				{
+					Name = IngredientNameTextBox.Text,
+					QuantityInStock = decimal.TryParse(QuantityTextBox.Text, out var qty) ? qty : 0,
+					Unit = UnitTextBox.Text,
+					PricePerUnit = decimal.TryParse(PriceTextBox.Text, out var price) ? price : 0
+				};
 
-			_ingredientRepository.Add(ingredient);
-			LoadIngredients();
+				_ingredientRepository.Add(ingredient);
+				LoadIngredients();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"An error occurred while adding the ingredient: {ex.Message}");
+			}
+		}
+
+		private void AddOrder_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				var order = new Order
+				{
+					OrderId = int.TryParse(OrderIDTextBox.Text, out var orderId) ? orderId : 0,
+					// CustomerName = CustomerNameTextBox.Text,
+					OrderDate = DateTime.TryParse(OrderDateTextBox.Text, out var orderDate) ? orderDate : DateTime.Now,
+					IngredientId = 0 /* get from the UI or select an ingredient */
+				};
+
+				_orderRepository.Add(order);
+				LoadOrders();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"An error occurred while adding the order: {ex.Message}");
+			}
+		}
+
+		private void AddSupplier_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				var supplier = new Supplier
+				{
+					Name = SupplierNameTextBox.Text,
+					ContactNumber = ContactTextBox.Text
+				};
+
+				_supplierRepository.Add(supplier);
+				LoadSuppliers();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"An error occurred while adding the supplier: {ex.Message}");
+			}
+
 		}
 	}
 }
